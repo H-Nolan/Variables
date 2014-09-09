39ce33e7befddb39dab8ce8f4f6756a324baa96d
Henry Nolan-Clutterbuck <44638@longroad.ac.uk>
2014-09-09 11:06:08 +0100

    Variable input

---
+++ Variable input.py
@@ -0,0 +1,10 @@
+#Henry Nolan-Clutterbuck
+#09-09-2014
+#Exercise 1.2 First programs
+#This is a variable that is storing the value entered by the user
+first_name = input("Please enter your first name: ")
+print(first_name)
+#this outputs the name in the in the format "Hi Bob!"
+print("Hi  {0}!".format(first_name))
+
+
