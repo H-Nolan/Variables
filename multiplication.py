eef80b6e0c29a6242ea8f2dbc042967f52fdf14d
Henry Nolan-Clutterbuck <44638@longroad.ac.uk>
2014-09-12 16:05:57 +0100

    Multiplication

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+#Henry Nolan-Clutterbuck
+#12/09/14
+#Multiplication Integers
+
+number_one=int(input("Please enter a number:"))
+number_two=int(input("Please add a 2nd number:"))
+number_three=int(input("Please add a third number:"))
+answer=int(number_one*number_one/number_three)
+print(answer)
