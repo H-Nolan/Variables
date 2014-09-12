ecc0113e081a10f2a1cb072ff3f1a634df75110b
Henry Nolan-Clutterbuck <44638@longroad.ac.uk>
2014-09-12 15:54:08 +0100

    Integer_addition

---
+++ Integer_Addition.py
@@ -0,0 +1,15 @@
+#Henry Nolan-Clutterbuck
+#12/09/2014
+#Integer addition exercise
+
+number_one=int(input("Please enter a number:"))
+
+number_two=int(input("Please add a 2nd number:"))
+
+number_three=int(input("Please add a third number:"))
+
+number_four=int(input("Please add your final number:"))
+
+answer=int(number_one+number_two+number_three+number_four)
+
+print(answer)
