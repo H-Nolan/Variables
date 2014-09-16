b044a9873ede2fe178db3bae865d2c878583d110
H-Nolan <44638@longroad.ac.uk>
2014-09-16 09:10:27 +0100

    Task 6.1 Markdown
    Task 6.1 Markdown

---
+++ Task 6.1.py
@@ -0,0 +1,9 @@
+#Henry Nolan-Clutterbuck
+#15/09/2014
+#Task 6.1
+number_one=int(input("Please enter a number:"))
+number_two=int(input("Please add a 2nd number:"))
+number_three=int(input("Please add a third number:"))
+answer=int(number_one+number_two+number_three)
+print("your answer is {0}!".format(answer))
+
