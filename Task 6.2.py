7b57bcc229b6ff4711f64f1ea7b79fb684d5d276
H-Nolan <44638@longroad.ac.uk>
2014-09-16 09:10:38 +0100

    Task 6.2 Markdown
    Task 6.2 Markdown

---
+++ Task 6.2.py
@@ -0,0 +1,8 @@
+#Henry Nolan-Clutterbuck
+#15/09/2014
+#Task 6.2
+number_one=int(input("Please enter a number:"))
+number_two=int(input("Please add a 2nd number:"))
+answer=int(number_one*number_two)
+print("Your answer is {0}!".format(answer))
+
