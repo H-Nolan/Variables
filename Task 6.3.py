7e8fe1624d2d562c21b9ded772a91ae4e3d85d8a
H-Nolan <44638@longroad.ac.uk>
2014-09-16 09:18:23 +0100

    Task 6.3 markdown
    Task 6.3 Markdown

---
+++ Task 6.3.py
@@ -0,0 +1,14 @@
+#Henry Nolan-Clutterbuck
+#15/09/2014
+#Task 6.3 markdown
+
+height = float(input("Please enter the depth of your swimming pool in metres: "))
+
+width = float(input("Please enter the width of your swimming pool in metres: "))
+
+length = float(input("Please enter the width of your swimming pool in metres: "))
+
+volume = length*width*height
+
+print("The volume of the swimming pool is {0}m^3".format(volume))
+      
